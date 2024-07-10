0b03451405cae4c9bcaa63758aa4eefc53572d36
aglairvta <aglairgb@hotmail.com>
2024-07-10 19:48:05 -0400

    Ajustes - Separação do html/js

--- 1. Calculadora/assets/js/main.js
+++ 1. Calculadora/assets/js/main.js
@@ -1,14 +1,36 @@
-const insert = (num) => {
+document.getElementById('limpar').addEventListener('click', clean);
+document.getElementById('voltar').addEventListener('click', back);
+
+document.getElementById('dividir').addEventListener('click', () => insert('/'));
+document.getElementById('multiplicar').addEventListener('click', () => insert('*'));
+document.getElementById('subtrair').addEventListener('click', () => insert('-'));
+document.getElementById('somar').addEventListener('click', () => insert('+'));
+
+document.getElementById('zero').addEventListener('click', () => insert('0'));
+document.getElementById('um').addEventListener('click', () => insert('1'));
+document.getElementById('dois').addEventListener('click', () => insert('2'));
+document.getElementById('tres').addEventListener('click', () => insert('3'));
+document.getElementById('quatro').addEventListener('click', () => insert('4'));
+document.getElementById('cinco').addEventListener('click', () => insert('5'));
+document.getElementById('seis').addEventListener('click', () => insert('6'));
+document.getElementById('sete').addEventListener('click', () => insert('7'));
+document.getElementById('oito').addEventListener('click', () => insert('8'));
+document.getElementById('nove').addEventListener('click', () => insert('9'));
+
+document.getElementById('ponto').addEventListener('click', () => insert('.'));
+document.getElementById('calcular').addEventListener('click', calcular);
+
+function insert (num) {
     let numero = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = numero + num;
-}
-const clean = () => document.getElementById('resultado').innerHTML = "";
-const back = () => {
+};
+function clean () {document.getElementById('resultado').innerHTML = ''};
+function back () {
     let resultado = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
-}
-const calcular = () => {
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1);
+};
+function calcular () {
     let resultado = document.getElementById('resultado').innerHTML;
-    if(resultado) document.getElementById('resultado').innerHTML = eval(resultado);
-    else document.getElementById('resultado').innerHTML = ""
-}
+    if (resultado) {document.getElementById('resultado').innerHTML = eval(resultado)}
+    else {document.getElementById('resultado').innerHTML = '';};
+};
