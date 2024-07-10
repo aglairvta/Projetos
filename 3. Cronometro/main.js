cd497ea4619ce3f6b15459aaa3313d5418b4bcd9
aglairvta <aglairgb@hotmail.com>
2024-07-10 18:36:54 -0400

    Adiciona - main/janela app

---
+++ 3. Cronometro/main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 500,
+    height: 640,
+    resizable: false,
+    autoHideMenuBar: true,
+    icon: path.join(__dirname, 'icon.png'),
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  mainWindow.loadFile('index.html');
+}
+
+app.on('ready', createWindow);
