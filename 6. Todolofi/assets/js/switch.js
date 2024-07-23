bb01b64f77e75fd992e046e25d1c2665adde4ea4
aglairvta <aglairgb@hotmail.com>
2024-07-23 02:00:54 -0400

    Ajuste - cores secundárias

--- 6. Todolofi/assets/js/switch.js
+++ 6. Todolofi/assets/js/switch.js
@@ -1,10 +1,11 @@
 const modoSwitch = document.getElementById('modoSwitch');
 const h1 = document.querySelector('.h1');
 const img = document.querySelector('.icon-principal');
-
+const container = document.querySelector('.container');
 modoSwitch.addEventListener('change', () => {
   if (modoSwitch.checked) {
-    document.body.style.backgroundColor = '#1d1e24';
+    document.body.style.backgroundColor = '#202020';
+    container.style.backgroundColor = '#c0c0c0'
     h1.style.color = '#fff';
     img.style.boxShadow = '2px 2px 2px rgba(255, 255, 255, 0.5)';
   } else {
